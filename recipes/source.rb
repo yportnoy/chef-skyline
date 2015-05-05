45de613b0e4348b6b83ab30cf49d500398aef664
Yasha Portnoy <yportnoy@optoro.com>
2015-05-05 15:22:13 -0400

    A few fixes

--- recipes/source.rb
+++ recipes/source.rb
@@ -3,14 +3,8 @@ include_recipe 'git'
 
 git node['skyline']['install_dir'] do
   repository node['skyline']['source']['repo']
-  reference node['skyline']['source']['reference']
+  reference node['skyline']['source']['commit_hash'] || node['skyline']['source']['reference']
   action :sync
-end
-
-execute 'reset-to-commit-version' do
-  command "git reset --hard #{node['skyline']['source']['commit_hash']}"
-  cwd node['skyline']['install_dir']
-  action :run
 end
 
 template "#{node['skyline']['install_dir']}/src/settings.py" do
